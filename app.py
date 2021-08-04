3d1b6cbbc080146302945ec763ec6df327b3b60c
Tetyana <iltaev2010@gmail.com>
2021-08-03 22:06:38 -0500

    added css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, render_template
 from stories import Story, story
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__) 
 
-app.config['SECRET_KEY'] = "oh-so-secret"
-debug = DebugToolbarExtension(app)
+# app.config['SECRET_KEY'] = "oh-so-secret"
+# debug = DebugToolbarExtension(app)
 
 STORIES = [
     (["place", "noun", "verb", "adjective", "plural_noun"],"Once upon a time in a long-ago {place}, there lived a large {adjective} {noun}. It loved to {verb} {plural_noun}."),
